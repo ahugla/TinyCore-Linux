41a5fcd0045f79ccd4f0f5154de8a04e91bc8430
ahugla <ahugla@vmware.com>
2020-11-30 13:26:13 +0100

    Update TCconf.sh

--- conf/TCconf.sh
+++ conf/TCconf.sh
@@ -48,8 +48,8 @@ sed -i '/box/a \loadkmap < /usr/share/kmap/azerty/fr-latin9.kmap\' /opt/bootsync
 # set password for tc and root
 # ----------------------------
 echo -e "$1\n$1" | passwd tc
-sudo echo -e "$1\n$1" | passwd root
-sudo echo '/etc/shadow' >> /opt/.filetool.lst         # pour etre backupé
+echo -e "$1\n$1" | passwd root
+echo '/etc/shadow' >> /opt/.filetool.lst         # pour etre backupé
 
 
 
@@ -58,21 +58,21 @@ sudo echo '/etc/shadow' >> /opt/.filetool.lst         # pour etre backupé
 # ---------------------------
 tce-load -wi openssh
 cd /usr/local/etc/ssh/
-sudo cp sshd_config.orig sshd_config
+cp sshd_config.orig sshd_config
 
-sudo chmod 777 sshd_config
+chmod 777 sshd_config
 echo "#ALEX CONF" >> sshd_config
 echo "PermitRootLogin yes" >> sshd_config
-sudo chmod 644 sshd_config
+chmod 644 sshd_config
 
-sudo /usr/local/etc/init.d/openssh start
+/usr/local/etc/init.d/openssh start
 
-sudo chmod 777 /opt/bootlocal.sh
-sudo echo '/usr/local/etc/init.d/openssh start &' >> /opt/bootlocal.sh  # demarrer SSH au boot
-sudo chmod 755 /opt/bootlocal.sh
+chmod 777 /opt/bootlocal.sh
+echo '/usr/local/etc/init.d/openssh start &' >> /opt/bootlocal.sh  # demarrer SSH au boot
+chmod 755 /opt/bootlocal.sh
 
-sudo echo '/opt/bootlocal.sh'  >> /opt/.filetool.lst    # pour etre backupé
-sudo echo '/usr/local/etc/ssh' >> /opt/.filetool.lst    # pour etre backupé
+echo '/opt/bootlocal.sh'  >> /opt/.filetool.lst    # pour etre backupé
+echo '/usr/local/etc/ssh' >> /opt/.filetool.lst    # pour etre backupé
 
 
 
